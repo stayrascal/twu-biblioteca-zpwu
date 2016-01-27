98449dce9d02066e28442ff21889c9057d9e31e5
rascal <stayrascal@gmail.com>
2016-01-27 21:38:03 +0800

    [ stayrascal ] rename method from should_display_welcome_message to console_should_display_welcome_message

--- test/com/twu/biblioteca/ApplicationTest.java
+++ test/com/twu/biblioteca/ApplicationTest.java
@@ -9,13 +9,14 @@ import static org.mockito.Mockito.verify;
 public class ApplicationTest {
 
     @Test
-    public void should_display_welcome_message() throws Exception {
+    public void console_should_display_welcome_message() throws Exception {
         ConsolePrinter console = mock(ConsolePrinter.class);
         Application app = new Application(console);
 
         app.start();
 
         verify(console).print("Welcome to Bangalore Public Library");
-
     }
+
+
 }
