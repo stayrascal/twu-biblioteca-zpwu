f534a9eca86b1b97103cf44a6ddc92ecbcd44f0b
rascal <stayrascal@gmail.com>
2016-01-28 17:19:03 +0800

    [ stayrascal ] add BookRepositoryTest

---
+++ test/com/twu/biblioteca/BookRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+public class BookRepositoryTest {
+
+    @Test
+    public void the_reutrn_list_size_should_be_1() throws Exception {
+        Book algebra = new Book(1, "algebra", "author1", "2012");
+        Book computer = new Book(2, "computer", "author2", "2013");
+        BookRepository bookRepository = new BookRepository(asList(new BookStock(algebra, 1), new BookStock(computer, 0)));
+
+        assertEquals(bookRepository.getAvailableBooks().size(), 1);
+        assertEquals(bookRepository.getAvailableBooks().get(0).getBook().getName(), "algebra");
+
+    }
+}
